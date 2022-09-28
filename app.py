98192220488002b2226cb7512b638d17c1ed46d1
Michał Banasiński <banasinski.michal@gmail.com>
2022-09-28 13:56:23 +0200

    cysty

--- app.py
+++ app.py
@@ -58,7 +58,9 @@ def update_charts(value):
             html.Div(dcc.Graph(figure=data_managment.fig_probability_60(str(value))))
        ])
     return children
-
+#app.css.append_css({
+#    "exsternal_url":
+#})
 if __name__=="__main__":
     #from waitress import serve
     #serve(app, host='0.0.0.0', port=8080)
